af69ce10a78ecfafa53b6d49d60dedfb5055095b
justmogen <justmogen@gmail.com>
2023-10-05 17:28:55 +0300

    concateneted string

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,5 +2,6 @@
 """ Basic annotations - concat """
 
 
-def concat(str1: string, str1: string) -> string:
-    return (str1 + str2)
+def concat(str1: str, str2: str) -> str:
+    """ Returns the concatenation of two strings """
+    return str1 + str2
