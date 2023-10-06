00534e4414fae883d1cab2ae852df36deb57161e
justmogen <justmogen@gmail.com>
2023-10-07 00:03:18 +0300

    duck-typing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Augment the following code with the correct duck-typed annotations:
     The types of the elements of the input are not know """
-from typing import Sequence , Any, Union
+from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
@@ -11,4 +11,3 @@ def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
         return lst[0]
     else:
         return None
-
