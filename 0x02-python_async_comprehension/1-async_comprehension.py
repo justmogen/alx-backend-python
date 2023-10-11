d7e5d4487d653d0f13e58e6954078e0c231a762d
justmogen <justmogen@gmail.com>
2023-10-11 20:46:52 +0300

    comprehesion in async

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Async Comprehensions """
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """ Async Comprehensions """
+    async_o = [i async for i in async_generator()]
+    return async_o
