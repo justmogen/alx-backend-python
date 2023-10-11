f69a3679ac23af03f4d51694622de0fa0ef1830e
justmogen <justmogen@gmail.com>
2023-10-11 21:11:01 +0300

    forgot to type-annotate the coroutine

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> float:
     """ Async Generator """
     for _ in range(10):
         await asyncio.sleep(1)
