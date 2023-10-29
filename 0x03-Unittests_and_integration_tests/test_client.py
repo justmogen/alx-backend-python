2f0164a784298db9f47fcb26e6516189635e6073
justmogen <justmogen@gmail.com>
2023-10-29 21:14:01 +0300

    integration test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,6 +8,7 @@ from utils import access_nested_map, get_json, memoize
 from client import GithubOrgClient
 import client
 from fixtures import TEST_PAYLOAD
+import requests
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -58,3 +59,54 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_has_licence(self, repo, license, expected):
         """ check licence availability """
         self.assertEqual(GithubOrgClient.has_license(repo, license), expected)
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ testing integration into github """
+
+    @classmethod
+    def setUpClass(cls):
+        org = TEST_PAYLOAD[0][0]
+        repos = TEST_PAYLOAD[0][1]
+        org_mock = Mock()
+        org_mock.json = Mock(return_value=org)
+        cls.org_mock = org_mock
+        repos_mock = Mock()
+        repos_mock.json = Mock(return_value=repos)
+        cls.repos_mock = repos_mock
+
+        cls.get_patcher = patch('requests.get')
+        cls.get = cls.get_patcher.start()
+
+        option = {cls.org_payload["repos_url"]: repos_mock}
+        cls.get.side_effect = lambda y: option.get(y, org_mock)
+
+    @classmethod
+    def tearDownClass(cls):
+        """ tear down testing patcher """
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """ testing the public repos """
+        git = GithubOrgClient("x")
+        self.assertEqual(git.org, self.org_payload)
+        self.assertEqual(git.repos_payload, self.repos_payload)
+        self.assertEqual(git.public_repos(), self.expected_repos)
+        self.assertEqual(git.public_repos("NONEXISTENT"), [])
+        self.get.assert_has_calls([call("https://api.github.com/orgs/x"),
+                                   call(self.org_payload["repos_url"])])
+
+    def test_public_repos_with_license(self):
+        """ public repos test """
+        y = GithubOrgClient("x")
+        self.assertEqual(y.org, self.org_payload)
+        self.assertEqual(y.repos_payload, self.repos_payload)
+        self.assertEqual(y.public_repos(), self.expected_repos)
+        self.assertEqual(y.public_repos("NONEXISTENT"), [])
+        self.assertEqual(y.public_repos("apache-2.0"), self.apache2_repos)
+        self.get.assert_has_calls([call("https://api.github.com/orgs/x"),
+                                   call(self.org_payload["repos_url"])])
